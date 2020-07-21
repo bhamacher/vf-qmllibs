fc59806ebb0430bf5141dbe01b79ac7c370eb534
bhamacher <b.hamacher@zera.de>
2020-07-21 13:55:56 +0200

    Added globaConfig lib
    global config is now available as lib.
    Link with: ZeraQml::globalconfig
    
    Signed-off-by: bhamacher <b.hamacher@zera.de>

--- libs/libs.cmake
+++ libs/libs.cmake
@@ -3,4 +3,5 @@ add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/uivectorgraphics_lib)
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/notificationmanager)
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/zeraFa)
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/zeraTranslation)
+add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/zeraGlobalConfig)
 
