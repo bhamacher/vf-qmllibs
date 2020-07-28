fb297702fd0cc1187c4feda052e7de1439277087
bhamacher <b.hamacher@zera.de>
2020-07-28 11:55:16 +0200

    Added zeraQwtChart lib.
    Signed-off-by: bhamacher <b.hamacher@zera.de>

--- libs/libs.cmake
+++ libs/libs.cmake
@@ -5,4 +5,5 @@ add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/zeraFa)
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/zeraTranslation)
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/zeraGlobalConfig)
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/zeraHelpers)
+add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/zeraQwtChart)
 
