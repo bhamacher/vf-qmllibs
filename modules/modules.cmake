523edcb0a7d0d244de780e289aab013e5c83cb3e
bhamacher <b.hamacher@zera.de>
2020-07-28 11:58:29 +0200

    removed old networkmanager from test executeable.
    Signed-off-by: bhamacher <b.hamacher@zera.de>

--- modules/modules.cmake
+++ modules/modules.cmake
@@ -1,2 +1,2 @@
-add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/networksettings_module)
+#add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/networksettings_module)
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/advancednetworkmanger_lib)
