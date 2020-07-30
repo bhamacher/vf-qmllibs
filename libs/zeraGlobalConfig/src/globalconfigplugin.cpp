c2f2334a14a1c86042925fb5233e7ac50736b357
bhamacher <b.hamacher@zera.de>
2020-07-30 09:14:12 +0200

    GlobalConfig: Removed JsonApi and use instead JsonSettingsQml lib now
    Signed-off-by: bhamacher <b.hamacher@zera.de>

--- libs/zeraGlobalConfig/src/globalconfigplugin.cpp
+++ libs/zeraGlobalConfig/src/globalconfigplugin.cpp
@@ -1,8 +1,8 @@
 #include "globalconfigplugin.h"
 #include <QCoreApplication>
 #include <QtQml/QtQml>
-#include "3rdparty/JsonSettingsQML/jsonglobalsettings.h"
-#include "3rdparty/JsonSettingsQML/jsonsettingsfile.h"
+#include <jsonglobalsettings.h>
+#include <jsonsettingsfile.h>
 
 
 void GlobalConfigPlugin::init()
@@ -15,8 +15,6 @@ void GlobalConfigPlugin::init()
 
 void GlobalConfigPlugin::registerTypes(const char* uri) {
     qmlRegisterSingletonType(QUrl("qrc:/src/qml/GlobalConfig.qml"), uri, 1, 0, "GC");
-    qmlRegisterType<JsonSettingsFile>("ZeraSettings", 1, 0, "ZeraSettings");
-    qmlRegisterType<JsonGlobalSettings>("ZeraSettings", 1, 0, "ZeraGlobalSettings");
 }
 
 
