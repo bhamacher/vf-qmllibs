f9e7d60482763779be3b4b654d3731ab58ee3e62
Andreas Müller <schnitzeltony@gmail.com>
2020-08-05 13:11:56 +0200

    Remove ZeraGlobalConfig
    It came from vf-declarative-gui and contains loads of application specific
    properties. Now that we
    
    * found a way for ZeraHelpers and ZeraComponents to live without and
    * moved GlobalConfig back
    
    we can say farewell to GlobalConfig here.
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- libs/libs.cmake
+++ libs/libs.cmake
@@ -3,7 +3,6 @@ add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/uivectorgraphics_lib)
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/notificationmanager)
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/zeraFa)
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/zeraTranslation)
-add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/zeraGlobalConfig)
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/zeraHelpers)
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/zeraQwtChart)
 
