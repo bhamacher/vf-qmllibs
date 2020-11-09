8ad5bf17b90f5d8e35502cccc5f20d4eaade4ffd
bhamacher <b.hamacher@zera.de>
2020-11-09 13:35:30 +0100

    Restructured zeracomponets_lib for qml debugging.
    All qml files are registered using qmldir now.
    registertypes is empty now!
    
    Signed-off-by: bhamacher <b.hamacher@zera.de>

---
+++ libs/zeracomponents_lib/src/qml/ZeraComponentsConfig/ZeraComponentsConfig.qml
@@ -0,0 +1,12 @@
+pragma Singleton
+import QtQuick 2.0
+import ZeraLocale 1.0
+
+// Container with defaults for all settings ZeraComponents uses. To make changes
+// or make settings persistent, bind them to other properties
+//
+Item {
+    // margin properties
+    property real standardTextHorizMargin: 8
+    property real standardTextBottomMargin: 8
+}
--- libs/zeracomponents_lib/src/zeracomponents.cpp
+++ libs/zeracomponents_lib/src/zeracomponents.cpp
@@ -9,16 +9,7 @@ void ZeraComponents::init()
 }
 
 void ZeraComponents::registerTypes(const char* uri) {
-    // Register config interface
-    qmlRegisterSingletonType(QUrl("qrc:/src/qml/ZeraComponentsConfig.qml"), "ZeraComponentsConfig", 1, 0, "ZCC");
-    // Register controls
-    qmlRegisterType(QUrl("qrc:/src/qml/ZButton.qml"),uri,1,0,"ZButton");
-    qmlRegisterType(QUrl("qrc:/src/qml/ZComboBox.qml"),uri,1,0,"ZComboBox");
-    qmlRegisterType(QUrl("qrc:/src/qml/ZDoubleValidator.qml"),uri,1,0,"ZDoubleValidator");
-    qmlRegisterType(QUrl("qrc:/src/qml/ZLineEdit.qml"),uri,1,0,"ZLineEdit");
-    qmlRegisterType(QUrl("qrc:/src/qml/ZSpinBox.qml"),uri,1,0,"ZSpinBox");
-    qmlRegisterType(QUrl("qrc:/src/qml/ZUnitComboBox.qml"),uri,1,0,"ZUnitComboBox");
-    qmlRegisterType(QUrl("qrc:/src/qml/ZVisualComboBox.qml"),uri,1,0,"ZVisualComboBox");
+
 }
 
 static void init(){
