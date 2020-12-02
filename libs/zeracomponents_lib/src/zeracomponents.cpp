cae3725c8088752f8298713c1139b3c5da96bec6
Andreas Müller <schnitzeltony@gmail.com>
2020-12-02 16:29:33 +0100

    ZeraComponents: Move file locations to prepare debugging
    * This is maximum consensus with [1] (that does not apply any more anyway).
      Before going on further, we have to decide how to continue. Particularly a
      SOLUTION for the 'as-needed' hack must be found.
    * A intermediate solution was necessary debug a bug in ZSpinBox (next patch)
    
    [1] https://github.com/ZeraGmbH/vf-qmllibs/pull/24
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- libs/zeracomponents_lib/src/zeracomponents.cpp
+++ libs/zeracomponents_lib/src/zeracomponents.cpp
@@ -10,15 +10,15 @@ void ZeraComponents::init()
 
 void ZeraComponents::registerTypes(const char* uri) {
     // Register config interface
-    qmlRegisterSingletonType(QUrl("qrc:/src/qml/ZeraComponentsConfig.qml"), "ZeraComponentsConfig", 1, 0, "ZCC");
+    qmlRegisterSingletonType(QUrl("qrc:/src/qml/ZeraComponentsConfig/ZeraComponentsConfig.qml"), "ZeraComponentsConfig", 1, 0, "ZCC");
     // Register controls
-    qmlRegisterType(QUrl("qrc:/src/qml/ZButton.qml"),uri,1,0,"ZButton");
-    qmlRegisterType(QUrl("qrc:/src/qml/ZComboBox.qml"),uri,1,0,"ZComboBox");
-    qmlRegisterType(QUrl("qrc:/src/qml/ZDoubleValidator.qml"),uri,1,0,"ZDoubleValidator");
-    qmlRegisterType(QUrl("qrc:/src/qml/ZLineEdit.qml"),uri,1,0,"ZLineEdit");
-    qmlRegisterType(QUrl("qrc:/src/qml/ZSpinBox.qml"),uri,1,0,"ZSpinBox");
-    qmlRegisterType(QUrl("qrc:/src/qml/ZUnitComboBox.qml"),uri,1,0,"ZUnitComboBox");
-    qmlRegisterType(QUrl("qrc:/src/qml/ZVisualComboBox.qml"),uri,1,0,"ZVisualComboBox");
+    qmlRegisterType(QUrl("qrc:/src/qml/ZeraComponents/ZButton.qml"),uri,1,0,"ZButton");
+    qmlRegisterType(QUrl("qrc:/src/qml/ZeraComponents/ZComboBox.qml"),uri,1,0,"ZComboBox");
+    qmlRegisterType(QUrl("qrc:/src/qml/ZeraComponents/ZDoubleValidator.qml"),uri,1,0,"ZDoubleValidator");
+    qmlRegisterType(QUrl("qrc:/src/qml/ZeraComponents/ZLineEdit.qml"),uri,1,0,"ZLineEdit");
+    qmlRegisterType(QUrl("qrc:/src/qml/ZeraComponents/ZSpinBox.qml"),uri,1,0,"ZSpinBox");
+    qmlRegisterType(QUrl("qrc:/src/qml/ZeraComponents/ZUnitComboBox.qml"),uri,1,0,"ZUnitComboBox");
+    qmlRegisterType(QUrl("qrc:/src/qml/ZeraComponents/ZVisualComboBox.qml"),uri,1,0,"ZVisualComboBox");
 }
 
 static void init(){
